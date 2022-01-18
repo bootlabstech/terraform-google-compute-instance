46a869b3865d44a988204d845b2e708810834094
Karthick Dharman <karthick.dharman@bootlabstech.com>
2022-01-18 22:23:13 +0530

    <No ID> <terraform template standardisation>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "default" {
   name         = var.name
   machine_type = var.machine_type
   zone         = var.zone
-  project      = var.project
+  project      = var.project_id
 
   tags = var.tags
 
@@ -14,7 +14,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
-	// Allow the instance to be stopped by terraform when updating configuration
+  // Allow the instance to be stopped by terraform when updating configuration
   allow_stopping_for_update = var.allow_stopping_for_update
 
   metadata_startup_script = var.enable_startup_script ? data.template_file.startup_script[0].rendered : null
@@ -27,23 +27,23 @@ resource "google_compute_instance" "default" {
     }
   }
 
-	dynamic "service_account" {
+  dynamic "service_account" {
     for_each = var.create_service_account ? [{}] : []
-    
+
     content {
-      email = google_service_account.default[0].email
+      email  = google_service_account.default[0].email
       scopes = var.service_account_scopes
     }
   }
 }
 
 data "template_file" "startup_script" {
-  count = "${var.enable_startup_script != null ? 1 : 0}"
-  template = "${file("startup.sh")}"
+  count    = var.enable_startup_script != null ? 1 : 0
+  template = file("startup.sh")
 }
 
 resource "google_service_account" "default" {
-	count = "${var.create_service_account ? 1 : 0}"
+  count        = var.create_service_account ? 1 : 0
   account_id   = format("%s-compute-instance", var.name)
   display_name = format("%s Compute Instance", var.name)
   project      = var.project
