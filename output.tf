3950a421ef1486bf36135141806bff9ab92a6941
Aravinda Kumar <aravind.kumar@bootlabstech.om>
2022-02-25 17:45:38 +0530

    fixing output

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "public_ip" {
 
 output "compute_instance_serviceaccount" {
   description = "The email/name of the compute instance service account"
-  value       = google_service_account.default[0] != null ? google_service_account.default[0].email : "service is account not created, enable create_service_account"
+  value       = length(google_service_account.default) > 0 ? google_service_account.default[0].email : "service is account not created, enable create_service_account"
 }
