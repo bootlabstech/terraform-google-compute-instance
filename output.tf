3684dca9c0623e38d1116672867219e7b0dcefaf
aravindakumarbootlabs <79306078+aravindakumarbootlabs@users.noreply.github.com>
2022-03-11 20:26:18 +0530

    removing output

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-output "public_ip" {
-  description = "The internet facing public ip for the compute incstance"
-  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
-}
-
 output "compute_instance_serviceaccount" {
   description = "The email/name of the compute instance service account"
   value       = length(google_service_account.default) > 0 ? google_service_account.default[0].email : "service is account not created, enable create_service_account"
