4ee0aa22da070c348428df27065138e4897967c5
Silpashree Acharya <silpashree@bootlabstech.com>
2023-11-23 21:41:52 +0530

    ip name

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "google_service_account" "default" {
 
 resource "google_compute_address" "static" {
   count        = var.address_type == "INTERNAL" ? (var.address == "" ? 0 : 1) : 1
-  name         = "compute-address"
+  name         = "${var.name_of_instance}-staticip" 
   project      = var.compute_address_project
   region       = var.compute_address_region
   address_type = var.address_type
